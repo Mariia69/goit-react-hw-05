ce9f3de3d7779fd25c78938054b59267f0a0afca
Mariia69 <141348185+Mariia69@users.noreply.github.com>
2024-05-19 16:15:31 +0300

    update app.jsx

--- scr/fetchFilms-api.js
+++ scr/fetchFilms-api.js
@@ -1,36 +1,51 @@
-import axios from "axios";
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-const tokenAPI = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTRiNWQ0ZTFjMmIyYjRhYTcwMDhhZGNlYTYxMmNiNCIsInN1YiI6IjY2NDllZThhNzNjZGI0NGJiZjJmYmJiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rib-RBb-yyYTdIPCyyzJBXu_7XyZiDX5mrMMnc0RzzA";
-axios.defaults.headers.common["Authorization"] = `Bearer ${tokenAPI}`;
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-export const fetchTrendingFilms = async () => {
-    const response = await axios.get(`trending/movie/day`);
-    return response.data;
-}
-
-export const fetchFilmsById = async (id) => {
-    const response = await axios.get(`movie/${id}`);
-    return response.data;
+const options = {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTRiNWQ0ZTFjMmIyYjRhYTcwMDhhZGNlYTYxMmNiNCIsInN1YiI6IjY2NDllZThhNzNjZGI0NGJiZjJmYmJiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rib-RBb-yyYTdIPCyyzJBXu_7XyZiDX5mrMMnc0RzzA',
+    Accept: 'application/json',
+  },
 };
 
-export const fetchFilms = async (query,page) => {
-    const response = await axios.get(`search/movie`, {
-        params: {
-            query,
-            page
-        }
-    });
-    return response.data;
-}
+const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 
-export const fetchReviews = async (id) => {
-    const response = await axios.get(`movie/${id}/reviews`);
-    return response.data;
-}
+export const fetchMovie = async () => {
+  const response = await axios.get(url, options).catch(err => console.log(err));
 
-export const fetchCredits = async (id) => {
-    const response = await axios.get(`movie/${id}/credits`);
-    return response.data;
+  return response.data.results;
+};
 
-}
+export const searchMovie = async query => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    options,
+  );
+  return response.data.results;
+};
+
+export const movieDetails = async movieId => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+    options,
+  );
+  return response.data;
+};
+
+export const movieCredits = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/credits?language=en-US`,
+    options,
+  );
+  return response.data.cast;
+};
+
+export const movieReview = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/reviews?language=en-US&page=1`,
+    options,
+  );
+  return response.data.results;
+};
